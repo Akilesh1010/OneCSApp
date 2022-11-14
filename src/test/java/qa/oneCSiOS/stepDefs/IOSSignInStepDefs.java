2aeb85f66bbb4e8bb20a53fcf6b5facc454b2046
nasar-cs <mohamednasar.syedhussain@charles-stanley.co.uk>
2022-11-14 10:28:03 +0530

    Nasar - 11Nov changes

--- src/test/java/qa/oneCSiOS/stepDefs/IOSSignInStepDefs.java
+++ src/test/java/qa/oneCSiOS/stepDefs/IOSSignInStepDefs.java
@@ -10,7 +10,9 @@ import io.cucumber.java.en.When;
 import qa.framework.assertions.AssertLogger;
 import qa.framework.dbutils.SQLDriver;
 import qa.framework.device.DeviceActions;
+import qa.framework.device.DeviceDriverManager;
 import qa.framework.utils.Reporter;
+import qa.oneCSAndroid.pages.OneCS_Mobile;
 import qa.oneCSAndroid.pages.SEA_HomeScreen;
 import qa.oneCSiOS.pages.DashboardScreen;
 import qa.oneCSiOS.pages.IOSSignInScreen;
@@ -20,19 +22,19 @@ public class IOSSignInStepDefs {
 	DeviceActions action = new DeviceActions(SQLDriver.getEleObjData("OneCSApp_iOS"));
 	IOSSignInScreen iOSSignInScreen = new IOSSignInScreen();
 	DashboardScreen dashboardScreen = new DashboardScreen();
-	
-	
+
+
 	@Given("user Clicks on {string} button for iOS")
 	public void user_Clicks_on_button(String btnName) throws InterruptedException {
-//		Thread.sleep(2000);
+		//		Thread.sleep(2000);
 		iOSSignInScreen.btnClickiOS(btnName);
 	}
-	
+
 	@When("user enters {string} and {string} for iOS")
 	public void user_enters_and(String userName, String pwd) {
 		iOSSignInScreen.userLoginiOS(userName, pwd);   
 	}
-	
+
 	@Then("user should see Sign in error message for iOS")
 	public void user_should_see_Sign_in_error_message() {
 		String actual = DeviceActions.getText((MobileElement) action.getElement("ERRORMSG_TXT"));
@@ -40,29 +42,91 @@ public class IOSSignInStepDefs {
 		AssertLogger.assertEquals(expected, actual, "iOS Error message do not match");
 		Reporter.addDeviceScreenshot("Login Screen", "Mobile App Login Screen");
 	}
-	
+
 	@Then("user should see {string} screen for iOS")
 	public void user_should_see_screen_for_iOS(String twoFA) throws InterruptedException {
 		Thread.sleep(3000);
 		Assert.assertTrue(action.isPresent(twoFA));
 		Reporter.addDeviceScreenshot("2FA Screen", "Mobile App Login Screen");		
 	}
-	
+
 	@Then("user get the SIX digit code displayed on screen and enters in TWOFA input field for iOS")
 	public void user_get_the_SIX_digit_code_displayed_on_screen_and_enters_in_TWOFA_input_field_for_iOS() throws InterruptedException {
 		iOSSignInScreen.input2FA();   
 	}
-	
+
 	@Then("user enters value in {string} field with {string} pin for iOS")
 	public void user_enters_value_in_field_with_pin_for_iOS(String fieldName, String value) throws InterruptedException {
-//		Thread.sleep(3000);
+		//		Thread.sleep(3000);
 		iOSSignInScreen.inputFieldiOS(fieldName, value);
-	    
+
 	}
-	
+
 	@Then("user sign in with valid username {string} and password {string} for iOS")
 	public void user_sign_in_with_valid_username_and_password(String userName, String pwd) {
 		iOSSignInScreen.signIniOS(userName, pwd);	
 	}
-	
+
+	@Then("user should see {string} in welcome screen for iOS")
+	public void user_should_see_in_welcome_screen_for_iOS(String txtOrBtnOrLink) {
+		Assert.assertTrue(action.isPresent(txtOrBtnOrLink));
+	}
+
+
+	@Given("user should see {string} text in welcome screen for iOS")
+	public void user_should_see_text_in_welcome_screen_for_iOS(String text) {
+		if(text.equalsIgnoreCase("CS_CREATE_A_SECURE")) {
+			Assert.assertEquals(iOSSignInScreen.getTextiOS("CS_WELCOME_LOGO"),DeviceActions.getTestData(text));
+		}
+		else {
+			Assert.assertEquals(iOSSignInScreen.getTextiOS("CREATE_ACCOUNT_ON_WEBSITE_LNK"), DeviceActions.getTestData(text));
+		}
+	}
+
+	@Then("user should be Navigated to the {string} website for iOS")
+	public void user_should_be_Navigated_to_the_website_for_iOS(String url) {
+		Assert.assertEquals(iOSSignInScreen.getTextiOS("BROWSER_URL"), DeviceActions.getTestData("CS_SECURE_URL"));	
+	}
+
+	@Then("user clicks on Back Button for iOS")
+	public void user_clicks_on_Back_Button_for_iOS() {
+		DeviceDriverManager.getDriver().navigate().back();
+	}
+
+	@Given("user should see {string} from Signin Screen for iOS")
+	public void user_should_see_from_Signin_Screen_for_iOS(String step1TitleTxt) {
+		Assert.assertEquals(iOSSignInScreen.getTextiOS("SIGN_IN_STEP_1_OF_4_TITLE"),step1TitleTxt);	
+	}
+
+	@Given("user should see {string} on the top left corner of the screen for iOS")
+	public void user_should_see_on_the_top_left_corner_of_the_screen_for_iOS(String closeBtn) {
+		Assert.assertTrue(action.isPresent(closeBtn));
+	}
+
+	@Given("user should see progress bar as {string} in signin screen for iOS")
+	public void user_should_see_progress_bar_as_in_signin_screen_for_iOS(String string) {
+
+	}
+
+	@Given("user should be displayed with {string} in sigin screen for iOS")
+	public void user_should_be_displayed_with_in_sigin_screen_for_iOS(String userNameAndPwdLabel) {
+		Assert.assertTrue(action.isPresent(userNameAndPwdLabel));
+
+	}
+
+	@Given("user should be displayed with {string} in signin screen for iOS")
+	public void user_should_be_displayed_with_in_signin_screen_for_iOS(String labelTxt) {
+		Assert.assertEquals(iOSSignInScreen.getTextiOS("PLEASE_ENTER_UN_PWD_LABEL_TXT"),labelTxt);	
+	}
+
+	@Given("user should see {string} text inside the username box for iOS")
+	public void user_should_see_text_inside_the_username_box_for_iOS(String editFieldTxt) {
+		Assert.assertEquals(iOSSignInScreen.getTextiOS("USERNAME_EDIT"),editFieldTxt);
+	}
+
+	@Given("user should see {string}  text inside the Password box for iOS")
+	public void user_should_see_text_inside_the_Password_box_for_iOS(String editFieldTxt) {
+		Assert.assertEquals(iOSSignInScreen.getTextiOS("PWD_EDIT"),editFieldTxt);
+	}
+
 }
